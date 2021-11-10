4ecac90fb1ae88a5608805f78249fb7cb75af072
umi729 <umer.zahid@revature.net>
2021-11-10 13:40:31 -0700

    after foolish act... updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {UserAuthModule} from './user-auth/user-auth.module'
 
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { NavBarComponent } from './nav-bar/nav-bar.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
    ],
   imports: [
     BrowserModule,
