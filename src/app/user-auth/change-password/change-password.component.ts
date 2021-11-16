ef5eab898409be7606639602bfa11a771da2cc50
umi729 <umer.zahid@revature.net>
2021-11-16 01:22:34 -0700

    new brancj2

--- src/app/user-auth/change-password/change-password.component.ts
+++ src/app/user-auth/change-password/change-password.component.ts
@@ -1,82 +1,89 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from '../services/user-service.service';
 import { Router } from '@angular/router';
-import{ UserInfo} from '../models/user-info'
+import { UserInfo } from '../models/user-info';
 import { MatDialog } from '@angular/material/dialog';
 import { Location } from '@angular/common';
-import {SuccessDialogComponent}  from '../../shared/dialogs/success-dialog/success-dialog.component'
-import { LoginInfo } from '../models/login-info';
-import {
-  FormControl,
-  FormGroup,
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { SuccessDialogComponent } from '../../shared/dialogs/success-dialog/success-dialog.component';
+import { FormControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Validation} from '../models/validation'
+
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css']
+  styleUrls: ['./change-password.component.css'],
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor(private service: UserServiceService, private router: Router, private dialog: MatDialog, private location: Location
-    ) { }
  
+  private validation= new Validation();
 
-    public form = new FormGroup({
+  constructor(
+    private service: UserServiceService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private location: Location,
+    private dialog: MatDialog,
+  ) { }
+
+  form: FormGroup = this.formBuilder.group({
      
-      password: new FormControl('', [
-        Validators.required,
-        Validators.maxLength(20),
-      ]),
-    });
+    password: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+    ]),
+    cpassword: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+    ]),
+  },
+  {
 
-
-    public hasError = (controlName: string, errorName: string) => {
-      return this.form.controls[controlName].hasError(errorName);
-    };
+    validator: this.validation.MustMatch('password', 'cpassword')
+}
   
-  
+  );
 
+  public hasError = (controlName: string, errorName: string) => {
+    return this.form.controls[controlName].hasError(errorName);
+  };
 
 
   dialogConfig = {
     height: '200px',
     width: '400px',
     disableClose: true,
-    data: { }
-  }
+    data: {},
+  };
 
-
-  usr:any|null=null;
-  changepass(newpass:UserInfo){
-   this.usr= sessionStorage.getItem("user");
-    const jsonobj= JSON.parse(this.usr);
+  usr: any | null = null;
+  changepass(newpass: UserInfo) {
+    this.usr = sessionStorage.getItem('user');
+    const jsonobj = JSON.parse(this.usr);
     console.log(jsonobj.userId);
-    
-    newpass.username=jsonobj.username;
-    this.service.geUpdatePass(jsonobj.userId, newpass).subscribe((pdata) => {
+
+    newpass.username = jsonobj.username;
+    this.service.geUpdatePass(jsonobj.userId, newpass).subscribe(
+      (pdata) => {
         console.warn(pdata);
-        let dialogRef = this.dialog.open(SuccessDialogComponent, this.dialogConfig);
+        let dialogRef = this.dialog.open(
+          SuccessDialogComponent,
+          this.dialogConfig
+        );
         //we are subscribing on the [mat-dialog-close] attribute as soon as we click on the dialog button
-        dialogRef.afterClosed()
-          .subscribe(result => {
-            this.location.back();
-          });
-    },
-    (error) => {                              //Error callback
-      console.error('error caught in change password component')
-     console.error(error);
-    
-      //throw error;   //You can also throw the error to a global error handler
-    });
-  }
-  ngOnInit(): void {
+        dialogRef.afterClosed().subscribe((result) => {
+          this.location.back();
+        });
+      },
+      (error) => {
+        //Error callback
+        console.error('error caught in change password component');
+        console.error(error);
 
-    
+        //throw error;   //You can also throw the error to a global error handler
+      }
+    );
   }
-
-  
+  ngOnInit(): void { }
 }
--- src/app/user-auth/reset-pass/reset-pass.component.ts
+++ src/app/user-auth/reset-pass/reset-pass.component.ts
@@ -3,6 +3,11 @@ import {Router} from '@angular/router';
 import {UserServiceService} from '../services/user-service.service'
 import {SignupInfo} from '../models/signup-info'
 import{ UserInfo} from '../models/user-info'
+import { MatDialog } from '@angular/material/dialog';
+import { Location } from '@angular/common';
+import { SuccessDialogComponent } from '../../shared/dialogs/success-dialog/success-dialog.component';
+import { FormControl,  FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Validation} from '../models/validation'
 
 @Component({
   selector: 'app-reset-pass',
@@ -10,17 +15,64 @@ import{ UserInfo} from '../models/user-info'
   styleUrls: ['./reset-pass.component.css'],
 })
 export class ResetPassComponent implements OnInit {
-  constructor(private service: UserServiceService, private router: Router) {}
   
-  usr:any|null=null;
+  private validation= new Validation();
+  
+  constructor(private service: UserServiceService, private router: Router,
+    private formBuilder: FormBuilder,
+   
+    private location: Location,
+    private dialog: MatDialog,) {}
+  
+    form: FormGroup = this.formBuilder.group({
+     
+      password: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(20),
+      ]),
+      cpassword: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(20),
+      ]),
+    },
+    {
+  
+      validator: this.validation.MustMatch('password', 'cpassword')
+  }
+    
+    );
+  
+    public hasError = (controlName: string, errorName: string) => {
+      return this.form.controls[controlName].hasError(errorName);
+    };
+  
+    dialogConfig = {
+      height: '200px',
+      width: '400px',
+      disableClose: true,
+      data: {},
+    };
+  
+  
+  
+    usr:any|null=null;
   changepass(newpass:UserInfo){
     this.usr=sessionStorage.getItem("temp");
     const obj = JSON.parse(this.usr);
-    console.log(obj);
+   // console.log(obj);
     this.service.geUpdatePass(obj.userId, newpass).subscribe((pdata) => {
         console.warn(pdata);
-        if(pdata!=null)
-        alert("password changed successfully")
+        if(pdata!=null){
+          let dialogRef = this.dialog.open(
+            SuccessDialogComponent,
+            this.dialogConfig
+          );
+          //we are subscribing on the [mat-dialog-close] attribute as soon as we click on the dialog button
+          dialogRef.afterClosed().subscribe((result) => {
+            this.location.back();
+          });
+        }
+       
         sessionStorage.removeItem("temp")
     },
     (error) => {                              //Error callback
